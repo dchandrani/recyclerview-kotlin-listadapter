843caa3d854910d8a8e9a321b6a1b58f85a78fce
Dhruv Chandrani <dhruv.chandrani@gmail.com>
2019-08-07 17:39:43 +0530

    Initial commit

---
+++ app/src/main/java/com/app/recyclerviewexamplekotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.recyclerviewexamplekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
